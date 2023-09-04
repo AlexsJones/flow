42d51f902035a7f34be0a1d600111d34fca4c154
Alex Jones <alexsimonjones@gmail.com>
2023-09-04 18:55:55 +0100

    empty
    Signed-off-by: Alex Jones <alexsimonjones@gmail.com>

---
+++ flow_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in flow_test.go
+// func TestFlowStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewFlowStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
